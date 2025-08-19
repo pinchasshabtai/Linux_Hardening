304ebf06d55115f924456ee0b951d9298c69ebc5
pinchasshabtai <pinchas2812@gmail.com>
2025-08-19 13:06:21 +0300

    Update show_lock_time.sh

--- Linux Hardening/installing_authentication/show_lock_time.sh
+++ Linux Hardening/installing_authentication/show_lock_time.sh
@@ -4,13 +4,15 @@ set -e
 
 FILE="/etc/verification/log_time.txt"
 
-if [[ ! -s "$FILE" ]]; then
+if systemctl is-active --quiet fapolicyd; then
     echo "The computer is already locked"
-    exit 0
+else
+    if [[ -s "$FILE" ]]; then
+        echo "The computer is already locked"
+        read -r epoch < "$FILE"
+        human_date=$(date -d "@$epoch" "+%Y-%m-%d %H:%M:%S")
+        echo "The computer will lock in: $human_date"
+    else
+        echo "The computer is unlocked by an administrator"
+    fi
 fi
-
-read -r epoch < "$FILE"
-
-human_date=$(date -d "@$epoch" "+%Y-%m-%d %H:%M:%S")
-
-echo "The computer will lock in: $human_date"
