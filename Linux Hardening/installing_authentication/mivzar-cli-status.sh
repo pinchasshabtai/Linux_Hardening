f1c701d86ce5a558f9365a182709688a6e355442
pinchasshabtai <pinchas2812@gmail.com>
2025-08-27 13:55:12 +0300

    fix bug cli status

--- Linux Hardening/installing_authentication/mivzar-cli-status.sh
+++ Linux Hardening/installing_authentication/mivzar-cli-status.sh
@@ -7,8 +7,7 @@ FILE="/etc/verification/log_time.txt"
 if systemctl is-active --quiet fapolicyd; then
     echo "The computer is already locked"
 else
-    if [[ -s "$FILE" ]]; then
-        echo "The computer is already locked"
+    if [[ -s "$FILE" ]]; then       
         read -r epoch < "$FILE"
         human_date=$(date -d "@$epoch" "+%Y-%m-%d %H:%M:%S")
         echo "The computer will lock in: $human_date"
